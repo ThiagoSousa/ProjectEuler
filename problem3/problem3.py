dc2e8e623971b40ae9b0d7922463778e5e22f820
Thiago Silveira <thiago.silveira@giance.ai>
2024-12-07 18:00:07 +0800

    Refactoring problem 3

--- problem3/problem3.py
+++ problem3/problem3.py
@@ -1,34 +1,5 @@
 import datetime
-import math
-
-
-def is_prime(n: int) -> bool:
-    """
-    Check if a number is prime. O(sqrt(n))
-    :param n: number
-    :return: True or False
-    """
-
-    if n < 2:
-        return False
-
-    for i in range(2, math.floor(math.sqrt(n))+1):
-        if n % i == 0:
-            return False
-    return True
-
-
-def next_prime(n: int) -> int:
-    """
-    Generate the next prime given an integer
-    :param n: number
-    :return: next larger prime than number n
-    """
-
-    n = n + 1
-    while not is_prime(n):
-        n += 1
-    return n
+from utils.prime_functions import next_prime
 
 start_time = datetime.datetime.now()
 number = 600851475143
